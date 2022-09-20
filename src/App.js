e517dd5fd0da92e76ff9dcb06463ed3ac8d2bc02
anshi06 <araghya6768@gmail.com>
2022-09-20 21:46:08 +0530

    FireBase database connection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback} from "react";
+import AddMovie from "./components/AddMovie";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -12,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch("https://react-http-9378f-default-rtdb.firebaseio.com/movies.json");
       if(!response.ok){
         throw new Error('Something went wrong!')
       }
@@ -54,9 +55,15 @@ function App() {
   //     releaseDate: '2021-05-19',
   //   },
   // ];
+  function addMovieHandler(movie){
+    console.log(movie)
+  }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}/>
+      </section>
       <section>
         <button onClick={fetchMoviesHAndler}>Fetch Movies</button>
       </section>
